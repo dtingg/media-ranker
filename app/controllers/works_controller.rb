6543aa2eeaac8b0ab913622fb7465ea2be594f50
Dianna Tingg <dtingg@gmail.com>
2019-10-14 21:07:35 -0700

    added new and create action and view for work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,4 +11,32 @@ class WorksController < ApplicationController
       return
     end
   end
+  
+  def new
+    @work = Work.new
+  end
+  
+  def create
+    # TODO double check this
+    # if params[:work].nil?
+    #   redirect_to new_work_path
+    #   return
+    # end
+    
+    @work = Work.new(work_params)
+    
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :new
+      return
+    end
+  end
+  
+  private
+  
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end  
 end
