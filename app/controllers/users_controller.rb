bbd2b4a42d57a6168d085b7da432eee65cb9360b
Dianna Tingg <dtingg@gmail.com>
2019-10-15 16:19:51 -0700

    created user model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(:username)
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+    
+    if @user.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def login_form
+    @user = User.new
+  end
+  
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+    
+    redirect_to root_path
+  end
+  
+  # def current
+  #   @current_user = User.find_by(id: session[:user_id])
+  #   unless @current_user
+  #     flash[:error] = "You must be logged in to see this page"
+  #     redirect_to root_path
+  #   end
+  # end
+  
+  def logout
+    session[:user_id] = nil
+    
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   root to: "homepages#index"
   
   resources :works
+  
+  get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  # get "/users/current", to: "users#current", as: "current_user"
 end
