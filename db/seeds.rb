ebe34ee1e03916a0ec3a748a86817cbfb4b03213
Dianna Tingg <dtingg@gmail.com>
2019-10-16 10:58:18 -0700

    added seeds for user and vote

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@
 
 require 'csv'
 
-STARTER_FILE = Rails.root.join('db', 'media-seeds.csv')
-puts "Loading raw work data from #{STARTER_FILE}"
+MEDIA_FILE = Rails.root.join('db', 'media-seeds.csv')
+puts "Loading raw work data from #{MEDIA_FILE}"
 
 work_failures = []
 
-CSV.foreach(STARTER_FILE, :headers => true) do |row|
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
   work = Work.new
   work.category = row['category']
   work.title = row['title']
@@ -32,3 +32,59 @@ end
 
 puts "Added #{Work.count} work records"
 puts "#{work_failures.length} works failed to save"
+
+USER_FILE = Rails.root.join('db', 'user-seeds.csv')
+puts "Loading raw user data from #{USER_FILE}"
+
+user_failures = []
+
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  
+  user.username = row['username']
+  user.joined = row['joined']
+  successful = user.save
+  
+  if !successful
+    user_failures << user
+    puts "Failed to save user: #{user.inspect}"
+  else
+    puts "Created user: #{user.inspect}"
+  end
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} users failed to save"
+
+VOTE_FILE = Rails.root.join('db', 'vote-seeds.csv')
+puts "Loading raw vote data from #{VOTE_FILE}"
+
+vote_failures = []
+
+CSV.foreach(VOTE_FILE, :headers => true) do |row|
+  vote = Vote.new
+  vote.user_id = row['user_id']
+  vote.work_id = row['work_id']
+  successful = vote.save
+  
+  if !successful
+    vote_failures << vote
+    puts "Failed to save vote: #{vote.inspect}"
+  else
+    puts "Created vote: #{vote.inspect}"
+  end
+end
+
+puts "Added #{Vote.count} vote records"
+puts "#{vote_failures.length} votes failed to save"
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+# puts "Manually resetting PK sequence on each table"
+# ActiveRecord::Base.connection.tables.each do |t|
+#   ActiveRecord::Base.connection.reset_pk_sequence!(t)
+# end
+
+# puts "done"
