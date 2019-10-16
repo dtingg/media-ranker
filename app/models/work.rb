9016ac293230c347a4d5cb9461fa2d5882098452
Dianna Tingg <dtingg@gmail.com>
2019-10-15 19:33:10 -0700

    created model and controller for vote

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  # has_many :votes, dependent: :nullify
+  has_many :votes, dependent: :nullify
   
   validates_inclusion_of :category, in: ["album", "book", "movie"]
   validates :title, presence: true, uniqueness: true
