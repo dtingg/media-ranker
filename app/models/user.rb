3df95d907f28689bed02622f51123d8baa7c8351
Dianna Tingg <dtingg@gmail.com>
2019-10-17 13:02:20 -0700

    fixed category selector width

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :votes, dependent: :nullify
+  has_many :votes, dependent: :destroy
   
   validates :username, presence: true
   validates :joined, presence: true
