fd13225ebe6c997ea3ee9a3138dda41bb5ea234d
Dianna Tingg <dtingg@gmail.com>
2019-10-17 18:22:17 -0700

    moved tests from user controller to votes controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,30 +1,4 @@
 require "test_helper"
 
 describe UsersController do
-  describe "logged in user" do
-    it "creates a vote if there is a logged in user" do
-      perform_login
-      
-      @work = Work.all.first
-      
-      expect { post work_upvote_path(@work.id) }.must_change "Vote.count", 1
-    end
-    
-    it "won't let a user vote for the same work twice" do
-      perform_login
-      
-      @work = Work.all.first
-      post work_upvote_path(@work.id) 
-      
-      expect { post work_upvote_path(@work.id) }.wont_change "Vote.count", 1
-    end
-  end
-  
-  describe "no logged in user" do
-    it "won't create a vote if there is no logged in user" do
-      @work = Work.all.first
-      
-      expect { post work_upvote_path(@work.id) }.wont_change "Vote.count", 1   
-    end
-  end
 end
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe VotesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "logged in user" do
+    it "creates a vote if there is a logged in user" do
+      perform_login
+      
+      @work = Work.all.first
+      
+      expect { post work_upvote_path(@work.id) }.must_change "Vote.count", 1
+    end
+    
+    it "won't let a user vote for the same work twice" do
+      perform_login
+      
+      @work = Work.all.first
+      post work_upvote_path(@work.id) 
+      
+      expect { post work_upvote_path(@work.id) }.wont_change "Vote.count", 1
+    end
+  end
+  
+  describe "no logged in user" do
+    it "won't create a vote if there is no logged in user" do
+      @work = Work.all.first
+      
+      expect { post work_upvote_path(@work.id) }.wont_change "Vote.count", 1   
+    end
+  end
 end
