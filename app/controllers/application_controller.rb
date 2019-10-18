8cd204864f33b27c344c042ae3c0d4073f368973
Dianna Tingg <dtingg@gmail.com>
2019-10-17 20:17:09 -0700

    added controller filter to find current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def find_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
 class VotesController < ApplicationController
+  before_action :find_current_user, only: [:upvote]
+  
   def upvote
-    @current_user = User.find_by(id: session[:user_id])
-    
     if @current_user.nil?
       flash[:warning] = "A problem occurred: You must log in to do that"
       redirect_back(fallback_location: root_path)
