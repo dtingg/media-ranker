4a131ef7a7fdcf97611f238eabfab3d9f951fd3a
Dianna Tingg <dtingg@gmail.com>
2019-10-18 19:49:02 -0700

    added user sort votes method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   
   validates :username, presence: true
   validates :joined, presence: true
+  
+  def sorted_votes
+    votes = self.votes
+    
+    sorted_votes = votes.sort_by do |vote|
+      vote.work.title
+    end
+    
+    return sorted_votes
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe User do
-  let (:new_user) { User.new(username: "Dianna", joined: "15 Oct 2019") }
+  let (:new_user) { User.new(username: "Heidi", joined: "15 Oct 2019") }
   
   describe "initialize" do
     it "can be instantiated" do
@@ -46,6 +46,26 @@ describe User do
       expect(new_user.valid?).must_equal false
       expect(new_user.errors.messages).must_include :joined
       expect(new_user.errors.messages[:joined]).must_equal ["can't be blank"]      
+    end
+  end
+  
+  describe "sorted_votes" do
+    it "returns votes sorted by title" do
+      test_user = users(:greg)
+      
+      sorted_votes = test_user.sorted_votes
+      
+      expect(sorted_votes).must_be_instance_of Array
+      expect(test_user.sorted_votes[0].work.title).must_equal "Goodbye Utopia"
+      expect(test_user.sorted_votes[1].work.title).must_equal "Green Forrester"
+      expect(test_user.sorted_votes[2].work.title).must_equal "Heart Mug"
+      expect(test_user.sorted_votes[3].work.title).must_equal "Hello Town"      
+    end
+    
+    it "return empty array if a user has no votes" do
+      new_user.save
+      
+      expect(new_user.sorted_votes).must_equal []      
     end
   end
 end
